cfa50d5901444287655ef79163fe5646dc5109d2
MDNICSRF <87891333+MDNICSRF@users.noreply.github.com>
2021-07-18 06:40:27 +0000

    File renamed.

---
+++ demo/src/main/java/com/daimajia/androidviewhover/demo/MainActivity.java
@@ -0,0 +1,22 @@
+
+package com.daimajia.androidviewhover.demo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.daimajia.androidviewhover.BlurLayout;
+
+public class MainActivity extends ActionBarActivity {
+
+    private Context mContext;
+    private BlurLayout mSampleLayout, mSampleLayout2, mSampleLayout3, mSampleLayout4;
