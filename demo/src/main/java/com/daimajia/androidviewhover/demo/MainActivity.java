4dfda92b56e1c5780f4c2dd04dcb9730335cd6d5
MDNICSRF <87891333+MDNICSRF@users.noreply.github.com>
2021-07-19 12:13:16 +0000

    Update TODO.txt

--- demo/src/main/java/com/daimajia/androidviewhover/demo/MainActivity.java
+++ demo/src/main/java/com/daimajia/androidviewhover/demo/MainActivity.java
@@ -20,3 +20,25 @@ public class MainActivity extends ActionBarActivity {
 
     private Context mContext;
     private BlurLayout mSampleLayout, mSampleLayout2, mSampleLayout3, mSampleLayout4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = this;
+        setContentView(R.layout.activity_main);
+        BlurLayout.setGlobalDefaultDuration(450);
+        mSampleLayout = (BlurLayout)findViewById(R.id.blur_layout);
+        View hover = LayoutInflater.from(mContext).inflate(R.layout.hover_sample, null);
+        hover.findViewById(R.id.heart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                YoYo.with(Techniques.Tada)
+                    .duration(550)
+                    .playOn(v);
+            }
+        });
+        hover.findViewById(R.id.share).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                YoYo.with(Techniques.Swing)
+                        .duration(550)
+                        .playOn(v);
