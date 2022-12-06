af3da02e45392d00d3bbf765924c86a93fc99da3
MDNICSRF <87891333+MDNICSRF@users.noreply.github.com>
2022-12-06 11:11:53 +0000

    fix version check

--- library/src/main/java/com/daimajia/androidviewhover/BlurLayout.java
+++ library/src/main/java/com/daimajia/androidviewhover/BlurLayout.java
@@ -3,3 +3,31 @@ package com.daimajia.androidviewhover;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.animation.Interpolator;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.daimajia.androidviewhover.proxy.AnimationProxy;
+import com.daimajia.androidviewhover.tools.Blur;
+import com.daimajia.androidviewhover.tools.Util;
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.ObjectAnimator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BlurLayout extends RelativeLayout {
+
+
+    private View mHoverView;
