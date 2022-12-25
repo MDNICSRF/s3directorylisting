b65f3b8ad5fda13667617b15beb62392160c696b
MDNICSRF <87891333+MDNICSRF@users.noreply.github.com>
2022-12-25 15:57:23 +0000

    nop

--- library/src/main/java/com/daimajia/androidviewhover/proxy/AnimationProxy.java
+++ library/src/main/java/com/daimajia/androidviewhover/proxy/AnimationProxy.java
@@ -11,3 +11,35 @@ import com.nineoldandroids.animation.Animator;
 public class AnimationProxy implements Runnable {
 
     private YoYo.AnimationComposer composer = null;
+    private Animator animator = null;
+
+    private long delay;
+    private long duration;
+    private boolean invisibleWhenDelaying;
+    private View targetView;
+    private Interpolator interpolator;
+
+    private long startTime = -1;
+
+    private AnimationProxy(View hoverView, int resId, Techniques technique, long duration, long delay, boolean invisibleWhenDelaying, Interpolator interpolator, Animator.AnimatorListener... listeners){
+        if(hoverView == null)
+            throw new IllegalStateException("Hover view is null");
+
+        View child = hoverView.findViewById(resId);
+
+        if(child == null)
+            throw new IllegalStateException("Can not find the child view");
+
+        if(duration < 0)
+            throw new IllegalArgumentException("Duration can not be less than 0");
+
+        if(delay < 0)
+            throw new IllegalArgumentException("Delay can not be less than 0");
+
+        this.composer = YoYo.with(technique).duration(duration).delay(delay).interpolate(interpolator);
+
+        this.targetView = child;
+        this.interpolator = interpolator;
+        this.delay = delay;
+        this.duration = duration;
+        this.invisibleWhenDelaying = invisibleWhenDelaying;
