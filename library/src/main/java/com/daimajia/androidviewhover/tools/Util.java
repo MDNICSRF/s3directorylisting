2b4b6279490b754cdb614e7dc4a30f6acf09e3c3
MDNICSRF <87891333+MDNICSRF@users.noreply.github.com>
2023-01-07 15:33:11 +0000

    Updated DTCoreText

--- library/src/main/java/com/daimajia/androidviewhover/tools/Util.java
+++ library/src/main/java/com/daimajia/androidviewhover/tools/Util.java
@@ -2,4 +2,6 @@ package com.daimajia.androidviewhover.tools;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.view.View
+import android.view.View;
+
+import com.nineoldandroids.v
