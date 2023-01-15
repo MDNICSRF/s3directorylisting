bd95dd42221d0d8843c5d0b7b2802074c6b3a447
MDNICSRF <87891333+MDNICSRF@users.noreply.github.com>
2023-01-15 21:40:12 +0000

    fixed logs

--- library/src/main/java/com/daimajia/androidviewhover/tools/Util.java
+++ library/src/main/java/com/daimajia/androidviewhover/tools/Util.java
@@ -13,4 +13,7 @@ public class Util {
             return null;
         Bitmap b = Bitmap.createBitmap( v.getWidth(), v.getHeight(), Bitmap.Config.ARGB_8888);
         Canvas c = new Canvas(b);
-  
+        v.draw(c);
+        return b;
+    }
+
